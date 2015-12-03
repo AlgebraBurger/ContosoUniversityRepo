3f37cc1dc8b436e0390d729c7bf8d435ba58063a
juliusbacosa <juliusbacosa@gmail.com>
2015-12-04 01:30:27 +0800

    - finishing

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -24,14 +24,35 @@ namespace ContosoUniversity.Controllers
             return View(await departments.ToListAsync());
         }
 
-        // GET: Department/Details/5
+        //// GET: Department/Details/5
+        //public async Task<ActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Department department = await db.Departments.FindAsync(id);
+        //    if (department == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(department);
+        //}
+
         public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Department department = await db.Departments.FindAsync(id);
+
+            // Commenting out original code to show how to use a raw SQL query.
+            //Department department = await db.Departments.FindAsync(id);
+
+            // Create and execute raw SQL query.
+            string query = "SELECT * FROM Department WHERE DepartmentID = @p0";
+            Department department = await db.Departments.SqlQuery(query, id).SingleOrDefaultAsync();
+
             if (department == null)
             {
                 return HttpNotFound();
